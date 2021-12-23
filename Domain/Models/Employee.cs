35228a3c86f519ddb41225ec511a1657d8a92fb8
Tahmin Fatiyev <tahmin.fatiyev@gmail.com>
2021-12-23 07:13:46 +0400

    Initial Project CompanyApp

---
+++ Domain/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Employee: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
